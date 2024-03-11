87eb82cc0d5aa07f4958d1ca2f72cb797a438f87
Mauricio Cardoso <mauricio.cds00@gmail.com>
2024-03-11 09:54:46 -0300

    feat: students average

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,51 @@ fn factorial() {
     println!("Factorial: {}", factorial);
 }
 
+fn students_avg() {
+    let mut averages_qtd = String::new();
+
+    println!("Input the students averages");
+
+    io::stdin()
+        .read_line(&mut averages_qtd)
+        .expect("Error reading input value");
+
+    let mut rec_sum = 0;
+    let mut apr_sum = 0;
+    let mut rep_sum = 0;
+    let mut i = 0;
+
+    while convert_str_to_int(&averages_qtd) > i {
+        let mut student_avg = String::new();
+
+        println!("Input student average");
+        io::stdin()
+            .read_line(&mut student_avg)
+            .expect("Error reading input value");
+
+        i += 1;
+
+        if convert_str_to_int(&student_avg) < 3 {
+            rep_sum += 1;
+        }
+
+        if convert_str_to_int(&student_avg) > 3 && convert_str_to_int(&student_avg) < 6 {
+            rec_sum += 1;
+        }
+
+        if convert_str_to_int(&student_avg) > 6 {
+            apr_sum += 1;
+        }
+    }
+
+    println!(
+        "Approved: {}, Recuperation: {}, Reproved: {}",
+        apr_sum, rec_sum, rep_sum
+    );
+}
+
 fn main() {
     // factorial();
+
+    students_avg();
 }
