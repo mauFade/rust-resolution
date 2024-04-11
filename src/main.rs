25ce85cc0002871929de7576a2c96df1021767a1
Mauricio Cardoso <mauricio.cds00@gmail.com>
2024-04-11 15:08:06 -0300

    feat: avg

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+fn avg(numbers: Vec<i32>) -> f64 {
+    let total_items = numbers.len() as f64;
+    let mut sum = 0;
+
+    for i in &numbers {
+        sum = sum + i;
+    }
+
+    println!("Total: {}, sum: {}", total_items, sum);
+
+    return (sum as f64) / total_items;
+}
+
+fn main() {
+    let avg = avg(vec![1, 2, 3, 4]);
+
+    println!("MEDIA: {}", avg);
+}
